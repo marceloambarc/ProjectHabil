b3d21edd2b45ba1c086341c2a66d5433ee015f4e
marceloambarc <marcelo.ambarc@gmail.com>
2021-01-07 10:11:09 -0300

    Proto Deprecated

--- server/database/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require("sequelize");
-
-const connection = new Sequelize('habil001', 'root', 'yhw7107', {
-    host : 'localhost',
-    dialect : 'mysql',
-    timezone : '-03:00'
-});
-
-module.exports = connection;
