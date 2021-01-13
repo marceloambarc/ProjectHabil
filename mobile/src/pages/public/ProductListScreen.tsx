38a77bd8f172958ec5d0c3aa24f0d51b253f66ba
marceloambarc <marcelo.ambarc@gmail.com>
2021-01-13 14:57:15 -0300

    List functional.

--- mobile/src/pages/public/ProductListScreen.tsx
+++ mobile/src/pages/public/ProductListScreen.tsx
@@ -1,148 +1,85 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, FlatList, 
-Image, TouchableOpacity, ActivityIndicator } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { Feather } from '@expo/vector-icons';
+import { View, Text, FlatList, StyleSheet, Image } from 'react-native';
 
-import api from '../../services/api';
+const baseURL = 'http://192.168.15.58:8080/products';
 
-const baseURL = api.get('products');
-
-function ProductsHeader(){
-  const navigation = useNavigation();
-
-  return(
-    <View style={styles.header}>
-      <TouchableOpacity style={styles.backBtn} onPress={() => navigation.navigate('Home')}>
-          <Feather name="arrow-left" size={28} color="#e82041" />
-      </TouchableOpacity>
-      <Text style={styles.headerTitle}>Meus Produtos</Text>
-    </View>
-  );
-}
-
-function renderItem({item} : {item: any}) {
-  return (
-    <View style={styles.listItem}>
-      
-      {/* IMAGE */}
-      <Image source={{uri:item.photo}}  style={styles.image} />
-
-      {/* CONTENT */}
-      <View style={styles.contentContainer}>
-        <Text style={styles.contentText}>{item.name}</Text>
-        <Text style={styles.contentTextPrice}>R$: {item.price}</Text>
-        <Text>{item.position}</Text>
-      </View>
-      
-      {/* SIDE BUTTONS */}
-      <View style={styles.btnContainer}>
-        <TouchableOpacity style={styles.btnModify}>
-            <Text style={{color:"red"}}>Deletar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btnModify}>
-            <Text style={{color:"gold"}}>Editar</Text>
-        </TouchableOpacity>
-      </View>
-
-    </View>
-  );
-}
-
-
-export default class App extends React.Component {
+export default class App extends Component {
   state = {
     data: [],
-    
-    loading: false,
-  }
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.loadRepositories();
   }
 
-  loadRepositories = async() => {
-    if (this.state.loading) return;
-
-    this.setState({ loading: true });
-
-    const response = await fetch(`${baseURL}`)
+  loadRepositories = async () => {
+    const response = await fetch(`${baseURL}`);
     const repositories = await response.json();
-
+    
     this.setState({
-      data: [ ...this.state.data, ...repositories.items],
-      loading: false,
+      data: [ ...this.state.data, ...repositories],
     });
   }
 
-  renderFooter = () => {
-    if (!this.state.loading) return null;
-
-    return (
-      <View style={styles.loading}>
-        <ActivityIndicator />
+  renderItem = ({ item }: {item: any}) => (
+    <View style={styles.listItem}>
+      {item.images.map((image:any) => {
+        return(
+          <Image source={{uri: image.url}} key={item.id.toString()} style={styles.image} />
+        );
+        })}
+          
+      <View style={styles.contentContainer}>
+        <Text style={styles.contentText}>{item.name}</Text>
+        <Text style={styles.contentTextPrice}>R$: {item.price}</Text>
       </View>
-    );
-  };
+    </View>
+  );
 
-
-  render(){
+  render() {
     return (
-      <View style={styles.container}>
-        <ProductsHeader />
-        <FlatList
-          style={{flex:1}}
-          data={this.state.data}
-          renderItem={renderItem}
-          keyExtractor={item => item.id}
-          onEndReached={this.loadRepositories}
-          onEndReachedThreshold={0.1}
-          ListFooterComponent={this.renderFooter}
-        />
-      </View>
+      <FlatList
+        style={styles.list}
+        data={this.state.data}
+        renderItem={this.renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
     );
   }
 }
 
 const styles = StyleSheet.create({
+  list: {
+    flex: 1,
+    marginTop: 30
+  },
   container: {
+    marginTop: 30,
     flex: 1,
     backgroundColor: '#F7F7F7',
   },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    width: '61%'
-  },
-  headerTitle: {
-    fontWeight: 'bold',
-    fontSize: 18,
-    justifyContent: 'center'
-  },
-  backBtn:{
-    marginTop:20,
-    marginLeft:20,
-    flex:1,
-    padding:10
-  },
   listItem:{
-    margin:10,
+    margin:7,
     padding:10,
     backgroundColor:'#FFF',
-    width:'80%',
+    width:'91%',
+    height: 100,
     flex:1,
     alignSelf:'center',
+
     flexDirection:'row',
     borderRadius:5
   },
   image:{
     width:60,
     height:60,
-    borderRadius:30
+    borderRadius:30,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   contentContainer:{
     alignItems: 'center',
+    justifyContent: 'center',
     flex: 1
   },
   contentText:{
@@ -151,19 +88,5 @@ const styles = StyleSheet.create({
   contentTextPrice:{
     color: 'red',
     fontWeight:'bold'
-  },
-  btnContainer:{
-    alignItems: 'center',
-    flex:1
-  },
-  btnModify:{
-    height:30,
-    width:50,
-    justifyContent:'center',
-    alignItems:'center'
-  },
-  loading: {
-    alignSelf: 'center',
-    marginVertical: 20,
   },
 });
