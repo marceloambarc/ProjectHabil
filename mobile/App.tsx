25d8766bf3ba6a34834466c1b9532e861ef57ae4
marceloambarc <marcelo.ambarc@gmail.com>
2021-01-21 11:29:10 -0300

    SearchBar light-content

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import Routes from './src/routes';
+import { StatusBar } from 'react-native';
 
 export default function App(){
   return(
-    <Routes />
+    <>
+      <StatusBar barStyle='light-content' />
+      <Routes />
+    </>
   );
 };
