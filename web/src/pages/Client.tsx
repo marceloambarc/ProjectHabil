5fdf6cda56751562b787de232ceb7a305a8a7c51
marceloambarc <marcelo.ambarc@gmail.com>
2021-07-05 18:01:31 -0300

    05/07 - 18:00

--- web/src/pages/Client.tsx
+++ web/src/pages/Client.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { FiDollarSign, FiLayers, FiBookOpen, FiArrowDownCircle, FiSearch } from 'react-icons/fi';
 import { IoCloseCircleOutline } from 'react-icons/io5';
 
 import api from '../services/api';
@@ -68,144 +67,147 @@ function Client(){
           </div>
 
           <div className="formContainer">
-            <div className="input-block">
-              <label htmlFor="name">Nome Fantasia:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={name}
-                onChange={event => setName(event.target.value)} 
-              />
+            <div className="formInput">
+              <div className="input-block">
+                <label htmlFor="name">Nome Fantasia:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={name}
+                  onChange={event => setName(event.target.value)} 
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="cnpj">CNPJ:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={cnpj}
+                  onChange={event => setCnpj(event.target.value)} 
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="ramo">Ramo:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={business}
+                  onChange={event => setBusiness(event.target.value)} 
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="telefone">Telefone/Whatsapp:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={telephone}
+                  onChange={event => setTelephone(event.target.value)} 
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="email">Email:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={email}
+                  onChange={event => setEmail(event.target.value)} 
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="endereço">Endereço:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={address}
+                  onChange={event => setAddress(event.target.value)} 
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="complemento">Complemento:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={complement}
+                  onChange={event => setComplement(event.target.value)} 
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="bairro">Bairro:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={district}
+                  onChange={event => setDistrict(event.target.value)} 
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="cidade">Cidade:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={city}
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="uf">UF:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={uf}
+                />
+              </div>
             </div>
-            <div className="input-block">
-              <label htmlFor="cnpj">CNPJ:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={cnpj}
-                onChange={event => setCnpj(event.target.value)} 
-              />
+          
+            <div className="formKeywords">
+              <div className="input-block">
+                <label htmlFor="about">Insira as palavras-chaves separadas por vírgula:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={keywords}
+                  onChange={event => setKeywords(event.target.value)} 
+                />
+              </div>
             </div>
-            <div className="input-block">
-              <label htmlFor="ramo">Ramo:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={business}
-                onChange={event => setBusiness(event.target.value)} 
-              />
+            <div className="formPassword">
+              <div className="input-block">
+                <label htmlFor="about">Senha:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={password}
+                  onChange={event => setPassword(event.target.value)} 
+                />
+              </div>
+              <div className="input-block">
+                <label htmlFor="about">Confirme Senha:</label>
+                <input 
+                  id="user" 
+                  maxLength={300}
+                  value={confirmPassword}
+                  onChange={event => setConfirmPassword(event.target.value)} 
+                />
+              </div>
             </div>
-            <div className="input-block">
-              <label htmlFor="telefone">Telefone/Whatsapp:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={telephone}
-                onChange={event => setTelephone(event.target.value)} 
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="email">Email:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={email}
-                onChange={event => setEmail(event.target.value)} 
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="endereço">Endereço:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={address}
-                onChange={event => setAddress(event.target.value)} 
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="complemento">Complemento:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={complement}
-                onChange={event => setComplement(event.target.value)} 
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="bairro">Bairro:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={district}
-                onChange={event => setDistrict(event.target.value)} 
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="cidade">Cidade:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={city}
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="uf">UF:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={uf}
-              />
-            </div>
-          </div>
-          <div className="formKeywords">
-            <div className="input-block">
-              <label htmlFor="about">Insira as palavras-chaves separadas por vírgula:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={keywords}
-                onChange={event => setKeywords(event.target.value)} 
-              />
-            </div>
-          </div>
-          <div className="formPassword">
-            <div className="input-block">
-              <label htmlFor="about">Senha:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={password}
-                onChange={event => setPassword(event.target.value)} 
-              />
-            </div>
-            <div className="input-block">
-              <label htmlFor="about">Confirme Senha:</label>
-              <input 
-                id="user" 
-                maxLength={300}
-                value={confirmPassword}
-                onChange={event => setConfirmPassword(event.target.value)} 
-              />
-            </div>
-          </div>
 
-          <div className="formImage">
-            <div className="close-button-container">
-              <button onClick={() => handleSelectImage()} id="button">
-                  <p>Selecione Foto</p>
-              </button>
+            <div className="formImage">
+              <div className="close-button-container">
+                <button onClick={() => handleSelectImage()} id="button">
+                    <p>Selecione Foto</p>
+                </button>
+              </div>
             </div>
-          </div>
 
-          <div className="formTerm">
-            Concordo com os <a href="/">termos de Uso</a>
-          </div>
+            <div className="formTerm">
+              Concordo com os <a href="/">termos de Uso</a>
+            </div>
 
-          <div className="formSend">
-            <div className="close-button-container">
-              <button onClick={() => handleCreate()} id="button">
-                  <p>Cadastrar</p>
-              </button>
+            <div className="formSend">
+              <div className="close-button-container">
+                <button onClick={() => handleCreate()} id="button">
+                    <p>Cadastrar</p>
+                </button>
+              </div>
             </div>
           </div>
 
